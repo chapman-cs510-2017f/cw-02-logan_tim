a8bd4ac647cd65ae8bb8a50e503a62077d299f97
Logan Gantner <user@a7f5c8f1ed7c>
2017-09-11 00:24:44 +0000

    debugged fibs.sh

--- fibs.sh
+++ fibs.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-n1 = 0
-n2 = 1
+n1=0
+n2=1
 if [ $1 -gt 0 ] 
 then
    for i in $(seq $1); do
-      n = $n1 + $n2
-      echo -n "$n "
-      n1 = n2
-      n2 = n
+      echo -n "$n2 "
+      n=$((n1+n2))
+      n1=$n2
+      n2=$n
    done;
 else
    exit 1
 fi
+echo      
       
-      
-      
+ 
