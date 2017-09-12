7a4c8be6bcebd2ac326bafea708d6afa9760884e
Logan Gantner <user@a7f5c8f1ed7c>
2017-09-12 03:35:03 +0000

    fixed minor issues with creatfiles.sh

--- createfiles.sh
+++ createfiles.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 mkdir tmpfiles;
-for i in $(seq 100); 
-	do
-	touch "tmpfiles/file$(printf "%03d" "$i").tmp";
-	echo "Temporary file $(printf "%03d" "$i")" > tmpfiles/file$(printf "%03d" "$i").tmp;
-done;
+for i in $(seq 100); do
+   touch "tmpfiles/file$(printf "%03d" "$i").tmp"
+   echo "Temporary file $(printf "%03d" "$i")" >> tmpfiles/file$(printf "%03d" "$i").tmp
+done
